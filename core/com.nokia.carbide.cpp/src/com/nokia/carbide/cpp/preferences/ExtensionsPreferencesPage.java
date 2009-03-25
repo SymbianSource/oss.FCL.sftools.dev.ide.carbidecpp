4d59b3aeedee1b7d2e4ee08810d5ee7aa4a40620
wpaul <none@none>
2009-03-25 12:24:24 -0500

    added copyright

--- core/com.nokia.carbide.cpp/src/com/nokia/carbide/cpp/preferences/ExtensionsPreferencesPage.java
+++ core/com.nokia.carbide.cpp/src/com/nokia/carbide/cpp/preferences/ExtensionsPreferencesPage.java
@@ -1,7 +1,19 @@
-/*******************************************************************************  
- * Copyright ©2007 Nokia Corporation. All rights reserved.                        
- *******************************************************************************/
-
+/*
+* Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).
+* All rights reserved.
+* This component and the accompanying materials are made available
+* under the terms of the License "Eclipse Public License v1.0"
+* which accompanies this distribution, and is available
+* at the URL "http://www.eclipse.org/legal/epl-v10.html".
+*
+* Initial Contributors:
+* Nokia Corporation - initial contribution.
+*
+* Contributors:
+*
+* Description: 
+*
+*/
 package com.nokia.carbide.cpp.preferences;
 
 import org.eclipse.jface.preference.PreferencePage;
