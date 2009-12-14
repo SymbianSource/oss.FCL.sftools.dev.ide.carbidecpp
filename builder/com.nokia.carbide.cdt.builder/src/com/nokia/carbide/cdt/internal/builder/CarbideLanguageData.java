f3aa4232255f7ce919230c8c085f4f1753d8f7cf
timkelly <none@none>
2009-12-14 10:24:12 -0600

    merge commit

--- builder/com.nokia.carbide.cdt.builder/src/com/nokia/carbide/cdt/internal/builder/CarbideLanguageData.java
+++ builder/com.nokia.carbide.cdt.builder/src/com/nokia/carbide/cdt/internal/builder/CarbideLanguageData.java
@@ -228,7 +228,13 @@ public class CarbideLanguageData extends CLanguageData {
 		if (oemDir.exists()) {
 			includeEntries.add(new CIncludePathEntry(new Path(oemDir.getAbsolutePath()), 0));
 		}
-
+		
+		// add the compiler include
+		IPath compilerPrefixPath = carbideBuildConfig.getCompilerPrefixFile();
+		if (compilerPrefixPath != null){
+			includeEntries.add(new CIncludePathEntry(new Path(compilerPrefixPath.removeLastSegments(1).toOSString()), 0));
+		}
+		
 		// cache the macros
 		macroEntries = new ArrayList<ICLanguageSettingEntry>(0);
 
