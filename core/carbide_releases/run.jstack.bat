63f36b3de3c0a6c7a1fc3658ce2b7a4e7f05620e
timkelly <none@none>
2010-02-03 10:56:46 -0600

    update to Carbide 2.5 EXE name
    --HG--
    branch : RCL_2_4

--- core/carbide_releases/run.jstack.bat
+++ core/carbide_releases/run.jstack.bat
@@ -1,6 +1,6 @@
 echo off
 
-rem This is a utility batch file to get stack information from the Carbide.c++.2.4.exe process.
+rem This is a utility batch file to get stack information from the Carbide.c++.2.5.exe process.
 rem In order to run it successfully, you will need to have a full JDK installed of version 1.6 or higher.
 rem See http://java.sun.com/javase/downloads/index.jsp for download information.
 rem If the Carbide process is found (and only one should be running) then stack information will be found in %OUTFILE%.
@@ -18,18 +18,18 @@ if not exist %JSTACK% GOTO END
 
 del %OUTFILE%
 
-rem echo Carbide.c++.2.4.exe >  %outfile%
+rem echo Carbide.c++.2.5.exe >  %outfile%
 
 echo
 echo Searching for Carbide.c++ process...
 
-FOR /F "tokens=2" %%i IN ('tasklist /FI "IMAGENAME eq  Carbide.c++.2.4.exe"
+FOR /F "tokens=2" %%i IN ('tasklist /FI "IMAGENAME eq  Carbide.c++.2.5.exe"
 /NH') DO @%JSTACK% %%i >  %OUTFILE%
 
 
 echo
 if exist %OUTFILE% echo Check for results in %OUTFILE%
-if not exist %OUTFILE% echo ERROR: Results not written. Is Carbide.c++.2.4.exe running?
+if not exist %OUTFILE% echo ERROR: Results not written. Is Carbide.c++.2.5.exe running?
 
 :END
 
