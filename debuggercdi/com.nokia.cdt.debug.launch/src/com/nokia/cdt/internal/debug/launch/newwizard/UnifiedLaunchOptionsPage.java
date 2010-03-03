5d72d6c973cc7bb37dc767d46af6b851605f49a4
dadubrow <none@none>
2010-03-03 08:27:25 -0600

    10809 - generating warning for missing sis file or missing exe

--- debuggercdi/com.nokia.cdt.debug.launch/src/com/nokia/cdt/internal/debug/launch/newwizard/UnifiedLaunchOptionsPage.java
+++ debuggercdi/com.nokia.cdt.debug.launch/src/com/nokia/cdt/internal/debug/launch/newwizard/UnifiedLaunchOptionsPage.java
@@ -17,6 +17,7 @@
 
 package com.nokia.cdt.internal.debug.launch.newwizard;
 
+import java.io.File;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 
@@ -31,6 +32,7 @@ import org.eclipse.swt.widgets.Control;
 
 import com.nokia.cdt.internal.debug.launch.LaunchPlugin;
 import com.nokia.cdt.internal.debug.launch.newwizard.IWizardSection.ISectionChangeListener;
+import com.nokia.cdt.internal.debug.launch.newwizard.LaunchWizardData.EExeSelection;
 import com.nokia.cpp.internal.api.utils.ui.WorkbenchUtils;
 
 /**
@@ -126,6 +128,13 @@ public class UnifiedLaunchOptionsPage extends WizardPage implements ISectionChan
 					status.getMessage()));
 			severity = Math.max(severity, status.getSeverity());
 		}
+		if (severity == IStatus.OK) {
+			IStatus status = getStatus();
+			if (!status.isOK()) {
+				builder.append(status.getMessage());
+				severity = status.getSeverity();
+			}
+		}
 		if (severity != 0 || builder.length() > 0) {
 			// error from one or more sections
 			pageStatus = new Status(severity, LaunchPlugin.PLUGIN_ID, builder.toString());
@@ -138,7 +147,7 @@ public class UnifiedLaunchOptionsPage extends WizardPage implements ISectionChan
 		
 		if (pageStatus != null && !pageStatus.isOK()) {
 			setMessage(pageStatus.getMessage(), severityToMsgType(pageStatus.getSeverity()));
-			setPageComplete(false);
+			setPageComplete(pageStatus.getSeverity() < IStatus.ERROR);
 		}
 	}
 
@@ -155,15 +164,30 @@ public class UnifiedLaunchOptionsPage extends WizardPage implements ISectionChan
 		}
 	}
 
-	/*
-	private String getSeverityTag(int severity) {
-		if (severity == IStatus.OK || severity == IStatus.INFO || severity == IStatus.CANCEL)
-			return "";
-		if (severity == IStatus.WARNING)
-			return "warning";
-		return "error";
+	protected IStatus getStatus() {
+		return checkBuildProducts();
 	}
-	*/
+
+	private IStatus checkBuildProducts() {
+		if (!data.isCurrentBuildBeforeLaunch()) {
+			// check sis files
+			String sisPath = data.getSisPath();
+			if (data.isInstallPackage() && 
+					sisPath != null && !new File(sisPath).exists()) {
+				return new Status(IStatus.WARNING, LaunchPlugin.PLUGIN_ID,
+						Messages.getString("UnifiedLaunchOptionsPage.SISFileMissingWarning")); //$NON-NLS-1$
+			}
+			// check launch file
+			if (data.getExeSelection().equals(EExeSelection.USE_PROJECT_EXECUTABLE) && 
+					!data.getExePath().toFile().exists()) {
+				return new Status(IStatus.WARNING, LaunchPlugin.PLUGIN_ID,
+						Messages.getString("UnifiedLaunchOptionsPage.ExeFileMissingWarning")); //$NON-NLS-1$
+				
+			}
+		}
+		
+		return Status.OK_STATUS;
+	}
 	
 	public void initializeSettings() {
 		for (IWizardSection section : sections) {
