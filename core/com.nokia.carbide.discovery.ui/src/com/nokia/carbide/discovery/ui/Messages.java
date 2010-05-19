e24186544a1600163606c164ab67807a83dc289a
dadubrow <none@none>
2010-05-19 15:35:14 -0500

    fix bugs, externalize strings, change some labels, add status line

---
+++ core/com.nokia.carbide.discovery.ui/src/com/nokia/carbide/discovery/ui/Messages.java
@@ -0,0 +1,20 @@
+package com.nokia.carbide.discovery.ui;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "com.nokia.carbide.discovery.ui.messages"; //$NON-NLS-1$
+	public static String DiscoveryView_CheckAllLabel;
+	public static String DiscoveryView_InstallLabel;
+	public static String DiscoveryView_MissingDirectoryURLError;
+	public static String DiscoveryView_RefreshLabel;
+	public static String DiscoveryView_StatusLineFmt;
+	public static String DiscoveryView_UncheckAllLabel;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
