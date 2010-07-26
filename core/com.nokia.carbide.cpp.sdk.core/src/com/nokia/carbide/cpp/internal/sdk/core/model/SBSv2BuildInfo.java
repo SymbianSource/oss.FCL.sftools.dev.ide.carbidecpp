30e88b099a90e446b54d2cd3222af4f73f349538
timkelly <none@none>
2010-07-26 17:32:53 -0500

    1) always try to rescan configs that have error messages. 2) remove some dead code
    --HG--
    branch : C3_BUILDER_WORK

--- core/com.nokia.carbide.cpp.sdk.core/src/com/nokia/carbide/cpp/internal/sdk/core/model/SBSv2BuildInfo.java
+++ core/com.nokia.carbide.cpp.sdk.core/src/com/nokia/carbide/cpp/internal/sdk/core/model/SBSv2BuildInfo.java
@@ -111,27 +111,6 @@ public class SBSv2BuildInfo implements ISBSv2BuildInfo {
 		return sbsv2FilteredConetxts;
 	}
 
-	private boolean isValidConfigForSDK(String alias) {
-		if (!alias.contains(".") && aliasToMeaningMap.get(alias) == null){
-			return false;
-		}
-		
-		// test variant
-		String[] configTokens = alias.split("\\.");
-		if (aliasToMeaningMap.get(configTokens[0]) == null){
-			return false;
-		}
-		
-		if (productList != null && productList.size() > 0)
-		for (String tok : configTokens){
-			if (productList.contains(tok)){
-				return true;
-			}
-		}
-		
-		return false;
-	}
-
 	private void initSBSv2BuildContextList(List<String> allowedConfigs) throws SBSv2MinimumVersionException {
 		List<String> filteredAliasList = new ArrayList<String>();
 		
@@ -162,7 +141,7 @@ public class SBSv2BuildInfo implements ISBSv2BuildInfo {
 		sbsv2FilteredConetxts.clear();
 		for (String alias : filteredAliasList) {
 			SBSv2ConfigQueryData configQueryData = SBSv2QueryUtils.getConfigQueryDataForSDK(sdk, alias);
-			if (configQueryData != null) {
+			if (configQueryData != null && configQueryData.getConfigurationErrorMessage().trim().length() == 0) {
 				ISBSv2BuildContext sbsv2Context = new BuildContextSBSv2(sdk, alias, configQueryData);
 				sbsv2FilteredConetxts.add(sbsv2Context);
 				processedAliasList.add(alias);
