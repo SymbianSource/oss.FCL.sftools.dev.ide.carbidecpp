d0e49b54c55d68f166ace33e52c30cc7df5842d3
dadubrow <none@none>
2010-07-29 15:31:01 -0500

    add portal page layer with sdk prefs

---
+++ core/com.nokia.carbide.cpp.sdk.ui/src/com/nokia/carbide/cpp/sdk/ui/SDKPreferencesPortalPageLayer.java
@@ -0,0 +1,48 @@
+/*
+* Copyright (c) 2010 Nokia Corporation and/or its subsidiary(-ies).
+* All rights reserved.
+* This component and the accompanying materials are made available
+* under the terms of the License "Eclipse Public License v1.0"
+* which accompanies this distribution, and is available
+* at the URL "http://www.eclipse.org/legal/epl-v10.html".
+*
+* Initial Contributors:
+* Nokia Corporation - initial contribution.
+*
+* Contributors:
+*
+* Description: 
+*
+*/
+package com.nokia.carbide.cpp.sdk.ui;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IEditorPart;
+
+import com.nokia.carbide.cpp.internal.sdk.ui.SDKPreferencePage;
+import com.nokia.carbide.internal.discovery.ui.extension.IPortalPageLayer;
+
+@SuppressWarnings("restriction")
+public class SDKPreferencesPortalPageLayer implements IPortalPageLayer {
+
+	private SDKPreferencePage preferencePage;
+
+	public Control createControl(Composite parent, IEditorPart part) {
+		preferencePage = new SDKPreferencePage();
+		preferencePage.createControl(parent);
+		return preferencePage.getControl();
+	}
+
+	public void init() {
+	}
+
+	public IActionBar[] createCommandBars(IEditorPart part, IActionUIUpdater updater) {
+		return null;
+	}
+
+	public void dispose() {
+		preferencePage.dispose();
+	}
+
+}
